4bc3b62e8ada62b0b74d9ae9c9e14cd90984c270
Lucas <lukas_boo@hotmail.com>
2018-09-23 13:58:33 -0300

    Initial Commit

---
+++ Expandable Menu Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Expandable Menu Example
+//
+//  Created by Lucas Daniel on 23/09/2018.
+//  Copyright © 2018 Smn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
