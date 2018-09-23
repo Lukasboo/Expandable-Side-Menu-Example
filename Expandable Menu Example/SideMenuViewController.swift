b73a73a8835535cc8adbb2707952a7f181ed25ff
Lucas <lukas_boo@hotmail.com>
2018-09-23 16:39:02 -0300

    first commit

---
+++ Expandable Menu Example/SideMenuViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SideMenuViewController.swift
+//  Expandable Menu Example
+//
+//  Created by Lucas Daniel on 23/09/2018.
+//  Copyright © 2018 Smn. All rights reserved.
+//
+
+import Foundation
